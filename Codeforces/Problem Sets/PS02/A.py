ce13d40b1bafcd73f8c963c1e84279ca0a04637e
Shayaan Qazi <122250924+shayaanhu@users.noreply.github.com>
2025-01-21 18:35:43 +0500

    PS2 A

---
+++ Codeforces/Problem Sets/PS02/A.py
@@ -0,0 +1,15 @@
+import math
+
+t = int(input())
+final = []
+
+for _ in range(t):
+    n = int(input())
+    nums = list(map(int, input().split()))
+    if math.sqrt(sum(nums)).is_integer():
+        final.append("YES")
+    else:
+        final.append("NO")
+
+for i in final:
+    print(i)
