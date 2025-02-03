83175e7b0e372ec9c3067fa1eedc291c684a7c85
Shayaan Qazi <122250924+shayaanhu@users.noreply.github.com>
2025-02-03 18:32:07 +0500

    x

--- Codeforces/Practice/900/2020A.py
+++ Codeforces/Practice/900/2020A.py
@@ -26,3 +26,16 @@ if os.path.exists("input.txt"):
     sys.stdin = open("input.txt", "r")
 
 # --- END TEMPLATE --- #
+
+for _ in range(inint()):
+    n, k = invars()
+    if k == 1:
+        print(n)
+        continue
+    opps = 0
+
+    while n > 0:
+        opps += n % k
+        n = n // k
+
+    print(opps) 
