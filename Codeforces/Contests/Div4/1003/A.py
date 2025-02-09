c6801ca52a576cd0571dc13285f4a2499b65c28c
Shayaan Qazi <122250924+shayaanhu@users.noreply.github.com>
2025-02-09 21:36:57 +0500

    1003

---
+++ Codeforces/Contests/Div4/1003/A.py
@@ -0,0 +1,33 @@
+# --- TEMPLATE 0 --- #
+
+# -- IMPORTS -- #
+
+import sys
+import os
+
+# -- INPUT SECTION -- #
+
+def inint():
+    return(int(input()))
+
+def inlist():
+    return(list(map(int,input().split())))
+
+def instr():
+    s = input()
+    return(list(s[:len(s) - 1]))
+
+def invars():
+    return(map(int,input().split()))
+
+# -- TESTING SECTION -- #
+
+if os.path.exists("input.txt"):
+    sys.stdin = open("input.txt", "r")
+
+# --- END TEMPLATE --- #
+for _ in range(inint()):
+    s = input()
+    n = len(s)
+    new_s = s[:n - 2]
+    print(new_s + "i")
