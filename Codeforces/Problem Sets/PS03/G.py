0da2bf808605b7e847e2f57ae95c1bb91c0f2851
Shayaan Qazi <122250924+shayaanhu@users.noreply.github.com>
2025-02-16 16:48:49 +0500

    Update G.py

--- Codeforces/Problem Sets/PS03/G.py
+++ Codeforces/Problem Sets/PS03/G.py
@@ -31,30 +31,18 @@ n, m, c = invars()
 a = inlist()
 b = inlist()
 
-# Precompute prefix sum for b.
-# pb[i] will be the sum of b[0] + b[1] + ... + b[i-1] (with pb[0]=0).
-pb = [0] * (m + 1)
+prefix = [0] * (m + 1)
 for i in range(m):
-    pb[i + 1] = pb[i] + b[i]
-
-# We'll use 0-indexing.
-# There are n-m+1 steps (from i=0 to i=n-m) that add b[0..m-1] to a sliding window.
-# For each message position k (0-indexed), it is affected by all steps i satisfying:
-#     i in [max(0, k-(m-1)), min(k, n-m)]
-# For each such i, the key element added is b[k - i].
-# Changing variable: let k' = k - i.
-# Then k' runs from A = k - min(k, n-m) to B = k - max(0, k-m+1).
-# So the total added to a[k] is pb[B+1] - pb[A].
+    prefix[i + 1] = prefix[i] + b[i]
 
 for k in range(n):
-    # i runs from I_low to I_high, where:
-    I_low = max(0, k - (m - 1))
-    I_high = min(k, n - m)
-    # Changing variable: k' = k - i.
-    # Then k' runs from:
-    A = k - I_high  # smallest k' when i is largest
-    B = k - I_low   # largest k' when i is smallest
-    addition = pb[B + 1] - pb[A]
+    low = max(0, k - (m - 1))
+    high = min(k, n - m)
+
+    aIndex = k - high
+    bIndex = k - low
+
+    addition = prefix[bIndex + 1] - prefix[aIndex]
     a[k] = (a[k] + addition) % c
 
 print(' '.join(map(str, a)))
