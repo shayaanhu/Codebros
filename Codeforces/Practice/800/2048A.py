fd1f1351a39b7615293ad313b3150e1c79e335a3
Shayaan Qazi <122250924+shayaanhu@users.noreply.github.com>
2025-02-22 13:53:24 +0500

    Create 2048A.py

---
+++ Codeforces/Practice/800/2048A.py
@@ -0,0 +1,45 @@
+# --- TEMPLATE 0 --- #
+
+# -- IMPORTS -- #
+
+import sys
+import os
+
+# -- INPUT SECTION -- #
+
+def inint():
+    return(int(input()))
+
+def inlist():
+    return(list(map(int,input().split())))
+
+def instr():
+    s = input()
+    return(list(s))
+
+def invars():
+    return(map(int,input().split()))
+
+# -- TESTING SECTION -- #
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+input_path = os.path.join(base_path, "input.txt")
+if os.path.exists(input_path):  
+    sys.stdin = open(input_path, "r")
+
+# Alternate 1
+# from pathlib import Path
+# base_path_2 = Path(__file__).parent
+    
+# Alternate 2
+# if os.path.exists("input.txt"):
+#     sys.stdin = open("input.txt", "r")
+
+# --- END TEMPLATE --- #
+
+for _ in range(inint()):
+    x = inint()
+    if x % 33 == 0:
+        print('YES')
+    else:
+        print('NO')
