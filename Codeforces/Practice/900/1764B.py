030e90ac27bb1a1ec91152ce513bc4b2edcafdad
Shayaan Qazi <122250924+shayaanhu@users.noreply.github.com>
2025-03-02 13:52:22 +0500

    Create 1764B.py

---
+++ Codeforces/Practice/900/1764B.py
@@ -0,0 +1,54 @@
+# --- TEMPLATE 0 --- #
+
+# -- IMPORTS -- #
+
+import sys
+import os
+import math
+import bisect
+from collections import Counter
+
+# -- INPUT SECTION -- #
+
+def inint():
+    return(int(input()))
+
+def inlist():
+    return(list(map(int,input().split())))
+
+def instr():
+    s = input()
+    return(list(s))
+
+def invars():
+    return(map(int,input().split()))
+
+# -- TESTING SECTION -- #
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+input_path = os.path.join(base_path, "input.txt")
+if os.path.exists(input_path):  
+    sys.stdin = open(input_path, "r")
+
+# Alternate 1
+# from pathlib import Path
+# base_path_2 = Path(__file__).parent
+    
+# Alternate 2
+# if os.path.exists("input.txt"):
+#     sys.stdin = open("input.txt", "r")
+
+# --- END TEMPLATE --- #
+
+for _ in range(inint()):
+    n = inint()
+    arr = inlist()
+
+    g = arr[0]
+    for num in arr[1:]:
+        g = math.gcd(g, num)
+
+    max_val = arr[-1]
+    result = max_val // g
+
+    print(result)
