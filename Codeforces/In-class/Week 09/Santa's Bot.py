8cb3e4261f051292c94cc7397f9bc1156dbb8f0f
Shayaan <122250924+shayaanhu@users.noreply.github.com>
2025-03-06 09:20:35 +0500

    Create Santa's Bot.py

---
+++ Codeforces/In-class/Week 09/Santa's Bot.py
@@ -0,0 +1,47 @@
+# --- TEMPLATE 0 --- #
+
+# -- IMPORTS -- #
+
+import sys
+import os
+import math
+import bisect
+from collections import Counter
+
+# -- INPUT SECTION -- #
+
+def inint():
+    return(int(input()))
+
+def inlist():
+    return(list(map(int,input().split())))
+
+def instr():
+    s = input()
+    return(list(s))
+
+def invars():
+    return(map(int,input().split()))
+
+# -- TESTING SECTION -- #
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+input_path = os.path.join(base_path, "input.txt")
+if os.path.exists(input_path):  
+    sys.stdin = open(input_path, "r")
+
+# Alternate 1
+# from pathlib import Path
+# base_path_2 = Path(__file__).parent
+    
+# Alternate 2
+# if os.path.exists("input.txt"):
+#     sys.stdin = open("input.txt", "r")
+
+# --- END TEMPLATE --- #
+
+p = 998244353
+# for i in range(6, 200):
+#     print(i, ((7 * pow(i, p - 2, p)) % p))
+
+print(((7 * pow(8, p - 2, p)) % p))
