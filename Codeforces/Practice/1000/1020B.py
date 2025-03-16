d82ef814f396b95a500e169ce88b51e2d2c60562
Shayaan Qazi <122250924+shayaanhu@users.noreply.github.com>
2025-03-16 15:15:20 +0500

    Create 1020B.py

---
+++ Codeforces/Practice/1000/1020B.py
@@ -0,0 +1,57 @@
+# --- TEMPLATE 0 --- #
+
+# -- IMPORTS -- #
+
+import sys
+import os
+import math
+import bisect
+from collections import Counter
+
+# -- INPUT SECTION -- #
+
+def inint():
+    return(int(input()))
+
+def inlist():
+    return(list(map(int,input().split())))
+
+def instr():
+    s = input()
+    return(list(s))
+
+def invars():
+    return(map(int,input().split()))
+
+# -- TESTING SECTION -- #
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+input_path = os.path.join(base_path, "input.txt")
+if os.path.exists(input_path):  
+    sys.stdin = open(input_path, "r")
+
+# Alternate 1
+# from pathlib import Path
+# base_path_2 = Path(__file__).parent
+    
+# Alternate 2
+# if os.path.exists("input.txt"):
+#     sys.stdin = open("input.txt", "r")
+
+# --- END TEMPLATE --- #
+
+n = inint()
+p = [x - 1 for x in inlist()]
+ans = [0] * n
+
+for a in range(n):
+    visited = [False] * n
+    current = a
+    while True:
+        if visited[current]:
+            ans[a] = current + 1
+            break
+        visited[current] = True
+        current = p[current]
+
+print(" ".join(map(str, ans)))
