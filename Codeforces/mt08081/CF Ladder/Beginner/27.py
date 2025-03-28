9ebe49da8e3becde11c85fe43cddf05da213d7d1
Muhammad Hassaan Tariq <mt08081@st.habib.edu.pk>
2025-03-28 19:49:29 +0500

    lol

---
+++ Codeforces/mt08081/CF Ladder/Beginner/27.py
@@ -0,0 +1,41 @@
+# lol
+
+# sieve of eratosthenes
+def sieve(n):
+    prime = [True for _ in range(n+1)]
+    p = 2
+    while p*p <= n:
+        if prime[p]:
+            for i in range(p*p, n+1, p):
+                prime[i] = False
+        p += 1
+
+    return prime
+
+primes = sieve(20001)
+
+for _ in range(int(input())):
+    n = int(input())
+    a = list(map(int, input().split()))
+    # a.sort()
+
+    summ = sum(a)
+
+    minn = min(a)
+    idx = a.index(minn)
+
+    if primes[summ]:
+        summ -= a[0]
+        print(n-1)
+        for i in range(n):
+            if i != idx:
+                print(i+1, end = ' ')
+            # print(i, end = ' ')
+        # print(n)
+        print()
+        continue
+
+    print(n)
+    for i in range(n):
+        print(i+1, end = ' ')
+    print()
