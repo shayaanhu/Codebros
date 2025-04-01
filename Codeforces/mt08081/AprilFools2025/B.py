769283e1e27b34563da803300698fe26f711ade3
Muhammad Hassaan Tariq <mt08081@st.habib.edu.pk>
2025-04-01 22:30:38 +0500

    lol this was fun

---
+++ Codeforces/mt08081/AprilFools2025/B.py
@@ -0,0 +1,39 @@
+# s = input()
+# n = int(s[-1])
+# if n == 0 or n >= 8:
+#     print(14)
+# elif n <= 5:
+#     print(2)
+# else:
+#     print(8)
+
+# Extract the round
+k = input().strip()
+s = int(k.split()[1])
+# 2, 8 and 14 are the only possible answers
+# s can be 1 to 10
+
+# if s >= 11:
+#     print(14)
+# elif s >= 6:
+#     print(8)
+# else:
+#     print(2)
+
+# if s <= 4:
+#     print(2)
+# elif s <= 7:
+#     print(8)
+# else:
+#     print(14)
+
+# if s == 1 or s == 2 or s == 3 or s == 4:
+#     print(2)
+# if s == 5 or s == 6 or s == 7:
+#     print(8)
+# if s == 8 or s == 9 or s == 10:
+#     print(14)
+
+# print(s)
+
+print(18) # print anything outside 0-16
---
+++ Codeforces/mt08081/AprilFools2025/C.py
@@ -0,0 +1,15 @@
+# # print(25)
+
+# s = input().strip()
+
+# # Check for the ascii value of the string
+# letters = sum(1 for c in s if c.isalpha())
+
+# # print(letters//2) # division gives 27 so I guess we sub 2 and see where that takes us
+# print(letters//2 - 2)
+
+# import random
+
+# print(random.randint(1, 10000000000))
+
+print(143) # Had to either brute force or use the status bar
