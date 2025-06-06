154c7affa6a95ccb23f7d714a288a9805e0c13bb
M Hassaan Tariq <mt08081@st.habib.edu.pk>
2025-06-06 17:49:03 +0500

    Update 30.py

--- Codeforces/mt08081/CF Ladder/Beginner/30.py
+++ Codeforces/mt08081/CF Ladder/Beginner/30.py
@@ -1,2 +1,9 @@
 # Casimir's String Solitaire
 
+from collections import Counter
+
+for _ in range(int(input())):
+    s = input()
+    cnt = Counter(s)
+    # print(cnt)
+    print('YES' if cnt['A'] + cnt['C'] == cnt['B'] else 'NO')
